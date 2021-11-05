57ac484f2af274dc0ecc541142db6019cbffc67f
Kristjan <krix@planet.ee>
2021-11-05 19:09:30 +0200

    cleanup

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import { AppProvider } from './context/appContext';
 import { UserProvider } from './context/userContext';
 import { DataProvider } from './context/dataContext';
 
